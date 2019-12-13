33a8737318f6bad9d4058b8bbc692132c4d8dfdd
shaopengwei <shaopengwei@baidu.com>
2019-12-13 19:04:01 +0800

    lib

---
+++ lib/logger.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2019/12/13 14:47
+@Author  : shaopengwei@baidu.com
+@File    : logger.py
+"""
+
+import sys
+import os
+import logging
+
+class Logger:
+    """
+    @desc: class logger
+    """
+
+    def __init__(self):
+        """
+        @:param self
+        """
---
+++ lib_ut/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2019/12/13 19:03
+@Author  : shaopengwei@baidu.com
+@File    : __init__.py
+"""
